6f9b3588792de0984d50f11746b0d0b010069b76
Rudranil Ghosh <ghoshrudranil8@gmail.com>
2024-03-14 17:09:09 +0530

    Create Weather App

---
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./WeatherApp.css";
+import search_icon from "../Assets/search.png";
+import drizzle_icon from "../Assets/drizzle.png";
+import rain_icon from "../Assets/rain.png";
+import snow_icon from "../Assets/snow.png";
+import wind_icon from "../Assets/wind.png";
+import humidity_icon from "../Assets/humidity.png";
+import cloud_icon from "../Assets/cloud.png";
+import clear_icon from "../Assets/clear.png";
+export const WeatherApp = () => {
+  return (
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" placeholder="search" className="cityInput" />
+        <div className="search-icon">
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt=""/>
+      </div>
+      <div className="weather-temp">24 c</div>
+      <div className="weather-location">India</div>
+      <div className="data-container">
+        <div className="element">
+            <img src="" alt="" className="icon"/>
+            <div className="data"><div className="humidity-percent">64%</div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default WeatherApp;
